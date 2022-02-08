34c71a0886ef08beaebb86b0fbc8dccf731e72e1
AQuaintExpression <iliemarian.daniel@gmail.com>
2022-02-08 20:12:30 +0200

    setting up project structure

---
+++ bubbleSortViz/bubble_sort_viz.py
@@ -0,0 +1,5 @@
+>>> import rich
+>>> print('[bold red]MARE SI ROSU[/] profit?')
+[bold red]MARE SI ROSU[/] profit?
+>>> from rich import print as rprint
+>>> rprint('[bold red]MARE SI ROSU[/] profit?')
