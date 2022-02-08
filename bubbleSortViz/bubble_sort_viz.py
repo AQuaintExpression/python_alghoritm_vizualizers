b8cdb281596cecaf3f0cf957c8dd0b7e33837992
AQuaintExpression <iliemarian.daniel@gmail.com>
2022-02-08 21:05:14 +0200

    implemented step by step sort with simple viz

--- bubbleSortViz/bubble_sort_viz.py
+++ bubbleSortViz/bubble_sort_viz.py
@@ -1,5 +1,33 @@
->>> import rich
->>> print('[bold red]MARE SI ROSU[/] profit?')
-[bold red]MARE SI ROSU[/] profit?
->>> from rich import print as rprint
->>> rprint('[bold red]MARE SI ROSU[/] profit?')
+from turtle import colormode
+from rich import print as rprint
+import time
+
+
+
+
+
+def bubble_sort_one_step(our_list, i=0, j=0, sorted=False):
+    # We go through the list as many times as there are elements
+    for i in range(len(our_list)):
+        # We want the last pair of adjacent elements to be (n-2, n-1)
+        for j in range(len(our_list) - 1):
+            if our_list[j] > our_list[j+1]:
+                # Swap
+                our_list[j], our_list[j+1] = our_list[j+1], our_list[j]
+                return sorted, i, j
+    sorted = True
+    return sorted, i, j
+
+
+our_list = [19, 13, 6, 2, 18, 8]
+sorted = False
+
+while not sorted:
+    sorted, i, j = bubble_sort_one_step(our_list)
+
+
+    colored_list = [str(x) for x in our_list]
+    colored_list = [x if index not in [j, j+1] else '[bold red]' + x + '[/]' for index, x in enumerate(colored_list)]
+
+    rprint(', '.join(colored_list))
+    time.sleep(1)
